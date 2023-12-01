62aa365e00b601e55f099bb93cb2638b36575488
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2023-12-02 00:33:29 +0700

    chore: add fonts & logo

---
+++ lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Poppins, Aoboshi_One } from "next/font/google";
+
+export const poppinsFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const aoboshiOneFont = Aoboshi_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
