463dbb4419fd42733be63813ab3f964ac10db1fd
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2023-12-12 23:06:28 +0700

    feat: add reusable spinner loading component

---
+++ components/loading/spinner.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+interface SpinnerProps {
+  className?: string;
+}
+
+export const Spinner = ({ className }: SpinnerProps) => {
+  return (
+    <svg
+      className={cn("animate-spin mr-3", className)}
+      viewBox="0 0 14 14"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#ffffff"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <g fill="none" fillRule="evenodd">
+          {" "}
+          <circle
+            cx="7"
+            cy="7"
+            r="6"
+            stroke="#ffffff"
+            strokeOpacity=".1"
+            strokeWidth="2"
+          ></circle>{" "}
+          <path
+            fill="#ffffff"
+            fillOpacity=".1"
+            fillRule="nonzero"
+            d="M7 0a7 7 0 0 1 7 7h-2a5 5 0 0 0-5-5V0z"
+          ></path>{" "}
+        </g>{" "}
+      </g>
+    </svg>
+  );
+};
