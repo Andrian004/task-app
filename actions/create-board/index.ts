24b423601988b61ee230cc8ca55bb174438ec597
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2023-12-15 22:25:21 +0700

    feat: improve create-board server action

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,21 +8,43 @@ import { InputType, ReturnType } from "./type";
 import { CreateBoard } from "./schema";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
-  const { userId } = auth();
+  const { userId, orgId } = auth();
 
-  if (!userId) {
+  if (!userId || !orgId) {
     return {
       error: "Unauthorized!",
     };
   }
 
-  const { title } = data;
+  const { title, image } = data;
+  const [imageId, imageThumbUrl, imageFullUrl, imageUserName, imageLinkHTML] =
+    image.split("|");
+
+  if (
+    !imageId ||
+    !imageThumbUrl ||
+    !imageFullUrl ||
+    !imageUserName ||
+    !imageLinkHTML
+  ) {
+    return {
+      error: "Missing fields. Failed create board.",
+    };
+  }
 
   let board;
 
   try {
     board = await db.board.create({
-      data: { title },
+      data: {
+        orgId,
+        title,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageUserName,
+        imageLinkHTML,
+      },
     });
   } catch (error) {
     return {
