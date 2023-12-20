2a83aa24fa27f584dfee976bd233f0aace533f9d
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2023-12-20 22:37:40 +0700

    feat: improve navbar(preview page)

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -6,7 +6,25 @@ export const Navbar = () => {
   return (
     <div className="flex items-center fixed top-0 w-full h-14 px-4 bg-white border-b shadow-sm">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-        <Logo />
+        <div className="flex items-center gap-x-2">
+          <div className="border-r">
+            <Logo />
+          </div>
+          <div className="flex gap-x-6">
+            <Link className="hover:text-rose-500 transition" href="/sign-in">
+              Features
+            </Link>
+            <Link className="hover:text-rose-500 transition" href="/sign-in">
+              Solutions
+            </Link>
+            <Link className="hover:text-rose-500 transition" href="/sign-in">
+              Pricing
+            </Link>
+            <Link className="hover:text-rose-500 transition" href="/sign-in">
+              Guide
+            </Link>
+          </div>
+        </div>
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
           <Button size="sm" variant="outline" asChild>
             <Link href="/sign-in">Login</Link>
